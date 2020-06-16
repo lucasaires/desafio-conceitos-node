660b9462da2ceaa631cd512b7127152c800b0798
Lucas Aires <lucas.aires@ccc.ufcg.edu.br>
2020-06-15 23:58:52 -0300

    desafio finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,92 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+  INDEX = 1;
+  NEGATIVE = -1;
+
+  const current_rep = repositories.findIndex(repository => repository.id === id);
+
+  if(current_rep === NEGATIVE){
+    return response.status(400).json({error:"Not Found"});
+  }
+  
+  const newRepository = {
+    id,
+    title,
+    url,                                      
+    techs,                                                                                                                                                                                                                                                      
+    likes : INDEX
+  }
+
+  newRepository.likes = repositories[current_rep].likes;
+
+  repositories[current_rep] = newRepository
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  NEGATIVE = -1;
+
+  const current_rep = repositories.findIndex(repository => repository.id === id);
+
+  if(current_rep === NEGATIVE){
+    return response.status(400).json({error:"Not found"});
+  }
+  repositories.splice(current_rep,1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  NEGATIVE = -1;
+  const current_rep = repositories.findIndex(repository => repository.id === id);
+
+  if(current_rep === NEGATIVE){
+    return response.status(400).json({error:"Not Found"});
+  }
+
+  const likes = (repositories[current_rep].likes + 1);
+
+  const value = repositories[current_rep];
+  
+  const newRepository = {
+    id: value.id,
+    title: value.title,
+    url: value.url,                       
+    techs: value.techs,                                                                                                                                                                                                                                                      
+    likes
+  }
+
+  repositories[current_rep] = newRepository
+
+  return response.json(newRepository);
 });
+
 
 module.exports = app;
